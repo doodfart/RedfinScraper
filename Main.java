27d961673e1f53fe0313c2c7bb18704fc99aca76
alexjchoi2000 <168559926+alexjchoi2000@users.noreply.github.com>
2024-05-01 19:52:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,27 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking user for their preferences
+        System.out.println("Enter minimum square footage:");
+        int minSqft = scanner.nextInt();  // Read integer input for minimum square footage
+
+        System.out.println("Enter minimum number of bedrooms:");
+        int minBedrooms = scanner.nextInt();  // Read integer input for minimum bedrooms
+
+        System.out.println("Enter maximum price:");
+        int maxPrice = scanner.nextInt();  // Read integer input for maximum price
+
+        scanner.close();
+
+
         RealtorsParser parser = new RealtorsParser(); // Create an instance of your parser class
         LinkedList<House> houses = parser.collectAllHouses(); // Use the method to collect all houses across pages
 
@@ -27,5 +44,18 @@ public class Main {
 
             System.out.println("-------------------------");
         }
+
+    }       
+        
+    // Filter houses based on user input
+    private static LinkedList<House> filterHouses(LinkedList<House> houses, int minSqft, int minBedrooms, int maxPrice) {
+        LinkedList<House> filteredHouses = new LinkedList<>();
+        for (House house : houses) {
+            if (house.sqft >= minSqft && house.bed >= minBedrooms && house.cost <= maxPrice) {
+                filteredHouses.add(house);
+            }
+        }
+        return filteredHouses;
+
+
     }
-}
