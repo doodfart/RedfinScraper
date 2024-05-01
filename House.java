daf03a1f5eb0d27afbe7c94c755459a827d4111d
doodfart <133732642+doodfart@users.noreply.github.com>
2024-05-01 18:33:49 -0400

    Update House.java

--- House.java
+++ House.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class House {
     // attributes
     public int cost;
@@ -8,21 +10,18 @@ public class House {
     public String houseURL;
     public String schoolDistrict;
     public String publicFacts;
-    public String style;
-    public String county;
-    public int lotSize;
     public int yearBuilt;
     public int marketCompetition;
     public int walkScore;
     public int transitScore;
     public int bikeScore;
-    public float buyerAgentFee;
+
+    public List<PriceData> priceHistory;
 
     // constructor
     public House(int cost, int bed, float bath, int sqft, String address, String houseURL,
-                 String schoolDistrict, String publicFacts, int lotSize, String style,
-                 String county, int yearBuilt, float buyerAgentFee, int marketCompetition,
-                 int walkScore, int transitScore, int bikeScore) {
+                 String schoolDistrict, int yearBuilt, int marketCompetition,
+                 int walkScore, int transitScore, int bikeScore, List<PriceData> priceHistory) {
         this.cost = cost;
         this.bed = bed;
         this.bath = bath;
@@ -30,15 +29,11 @@ public class House {
         this.address = address;
         this.houseURL = houseURL;
         this.schoolDistrict = schoolDistrict;
-        this.publicFacts = publicFacts;
-        this.lotSize = lotSize;
-        this.style = style;
-        this.county = county;
         this.yearBuilt = yearBuilt;
-        this.buyerAgentFee = buyerAgentFee;
         this.marketCompetition = marketCompetition;
         this.walkScore = walkScore;
         this.transitScore = transitScore;
         this.bikeScore = bikeScore;
+        this.priceHistory = priceHistory;
     }
 }
